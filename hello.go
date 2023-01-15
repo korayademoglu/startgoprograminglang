91770cde1bfe25dfb3f99a9a1dc81000dc0ac617
korayademoglu <korayademoglu@gmail.com>
2023-01-15 23:32:18 +0300

    modul and package

--- hello.go
+++ hello.go
@@ -1,25 +1,7 @@
 package main
 
-import "fmt"
+import "golesson/variables"
 
 func main() {
-	var metin string = "Merhaba Dunya"
-	var kdv int = 18
-	fmt.Println("Hello world")
-	fmt.Println(metin)
-	fmt.Println(100 + (100 * kdv / 100))
-
-	var kdv2 float32 = 0.1
-	fmt.Println(100 + 100*kdv2)
-
-	kdv3 := 25
-	fmt.Println(kdv3)
-	fmt.Printf("veri tipi: %T\n", kdv3)
-
-	var durum bool
-
-	var ad1 = "Koray"
-	var ad2 = "Koray"
-	durum = ad1 == ad2
-	fmt.Println(durum)
+	variables.Demo1()
 }
