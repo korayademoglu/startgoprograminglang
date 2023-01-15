ce4138cecd15d813c0c15c5ea9127e80593c894d
korayademoglu <korayademoglu@gmail.com>
2023-01-15 23:10:43 +0300

    veri tipi

--- hello.go
+++ hello.go
@@ -7,5 +7,12 @@ func main() {
 	var kdv int = 18
 	fmt.Println("Hello world")
 	fmt.Println(metin)
-	fmt.Println(100 + 100*kdv/100)
+	fmt.Println(100 + (100 * kdv / 100))
+
+	var kdv2 float32 = 0.1
+	fmt.Println(100 + 100*kdv2)
+
+	kdv3 := 25
+	fmt.Println(kdv3)
+	fmt.Printf("veri tipi: %T", kdv3)
 }
